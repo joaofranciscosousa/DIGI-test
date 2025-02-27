decb0a7c7ceef65c301608930f505eebfa62d141
João Francisco <jfsousacarvalho@gmail.com>
2025-02-26 23:32:16 -0300

    add: [productsList] first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-layout>
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title> Loja Digital DIGI </q-toolbar-title>
+
+        <div><q-icon name="shopping_cart" size="30px" /></div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {},
+
+  data() {
+    return {}
+  },
+
+  methods: {},
+})
+</script>
